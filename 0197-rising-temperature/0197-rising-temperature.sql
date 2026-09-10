d0390b392e4c8fab1af35a6811b771407d100088
Vinoth Kumar <vinothkumarshivasankari@gmail.com>
2026-09-11 01:10:59 +0530

    Time: 473 ms (77.4%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select a.id from weather a join weather b on datediff(a.recorddate,b.recorddate) = 1 where b.temperature < a.temperature
+select a.id from weather a join weather b on b.temperature < a.temperature where datediff(a.recorddate,b.recorddate) = 1 
